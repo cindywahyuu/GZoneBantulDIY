33feb82866a0f914eb2304a114d4c12d51f61464
cindywahyuu <cindywahyukarisma@mail.ugm.ac.id>
2026-05-29 18:11:06 +0700

    first commit

---
+++ data/KantorBPBD_8.js
@@ -0,0 +1,1 @@
+var json_KantorBPBD_8 = {"type":"FeatureCollection","name":"KantorBPBD_8","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Id":1.0,"Keterangan":null},"geometry":{"type":"Point","coordinates":[110.321857235192553,-7.899501144038693]}}]}
